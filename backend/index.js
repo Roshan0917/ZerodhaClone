c3255afce221b47c0dda65ac59c05c58cad1852e
Roshan0917 <trock0917@gmail.com>
2026-07-08 12:35:46 +0530

    backend deployment setup

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,12 @@ const PORT = process.env.PORT || 3002;
 const MONGO_URI = process.env.MONGO_URL;
 
 // ================= GLOBAL MIDDLEWARE =================
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods:["GET","POST","PUT","DELETE"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
