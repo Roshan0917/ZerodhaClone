a50a53306b97b50903adf08852dd76043ffa0898
Roshan0917 <trock0917@gmail.com>
2026-07-09 01:27:16 +0530

    Fix unused imports in Dashboard

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -1,13 +1,5 @@
 import React from "react";
-
-import Apps from "./Apps";
-import Funds from "./Funds";
-import Holdings from "./Holdings";
-import Orders from "./Orders";
-import Positions from "./Positions";
 import Summary from "./Summary";
-import OpenAccount from "./OpenAccount";
-import AdminPanel from "./AdminPanel";
 import WatchList from "./WatchList";
 import { GeneralContextProvider } from "./GeneralContext";
 
